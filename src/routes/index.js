14f41615f36bd3214325c4dfe2119a26bd97f8fa
zzheka <e.beloded@gmail.com>
2016-04-16 14:38:13 +0300

    less comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,12 @@ const path = require('path');
 const fs = require('fs');
 
 function loadRoutes(app) {
-  fs.readdirSync(__dirname).map(file=> {
-    /* If its the current file ignore it */
+  fs.readdirSync(__dirname).map(file=>{
     if (file === 'index.js') return;
     if (file === 'root.js') {
       app.use('/', require(__dirname + '/'.file));
       return;
     }
-
     let moduleName = file.split('.');
     if (moduleName.pop() == '.js') {
       app.use(moduleName.join('.'), require(__dirname + '/'.file));
