17493de3185be87527b138de500092b8c8e9270f
Andrii Hrachov <andrii.hrachov@gettyimages.com>
2016-05-14 09:02:56 +0300

    Fix routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,12 @@ function loadRoutes(app) {
   fs.readdirSync(__dirname).map(file=>{
     if (file === 'index.js') return;
     if (file === 'root.js') {
-      app.use('/', require(__dirname + '/'.file));
+      app.use('/', require(__dirname + '/' + file));
       return;
     }
     let moduleName = file.split('.');
     if (moduleName.pop() == '.js') {
-      app.use(moduleName.join('.'), require(__dirname + '/'.file));
+      app.use(moduleName.join('.'), require(__dirname + '/' + file));
     }
   });
 }
